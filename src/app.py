f89083e28522e0d9bda98618bfd6841246c7a705
Zak (Lenovo) <zakcatherall@outlook.com>
2022-01-09 11:55:30 +0000

    Started creating api docs on index

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 from cache import cache
 from config import config
@@ -14,7 +14,7 @@ app.register_blueprint(list_item_blueprint, url_prefix='/list/item')
 
 @app.route('/')
 def index():
-    return "OK"
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8000, debug=True)
